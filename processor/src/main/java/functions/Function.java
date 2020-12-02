b72d28a7312861c8f15b8d1e42baee448edf2250
Matej Vasek <mvasek@redhat.com>
2020-12-02 22:38:09 +0100

    retrying for face api

--- processor/src/main/java/functions/Function.java
+++ processor/src/main/java/functions/Function.java
@@ -1,6 +1,8 @@
 package functions;
 
 
+import java.net.ConnectException;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import java.util.*;
@@ -13,10 +15,17 @@ import io.quarkus.funqy.Context;
 import io.quarkus.funqy.Funq;
 import io.quarkus.funqy.knative.events.CloudEvent;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.subscription.UniEmitter;
+import io.vertx.core.Vertx;
+
+import javax.inject.Inject;
 
 public class Function {
 
     private static final String API_KEY = "dcc086604d47489e86bd6d216aa5b09b";
+
+    @Inject
+    Vertx vertx;
 
     private static final List<FaceAttributeType> attributes;
     static {
@@ -26,34 +35,46 @@ public class Function {
         attributes = Collections.unmodifiableList(attrs);
     }
 
-    @Funq
-    public Uni<Output[]> function(Input input, @Context CloudEvent cloudEvent) throws Throwable {
-        return Uni.createFrom().emitter(emitter -> {
 
-            FaceAPI faceAPI = FaceAPIManager.authenticate("https://boson.cognitiveservices.azure.com/face/v1.0", API_KEY);
-            faceAPI
-                    .withAzureRegion(AzureRegions.EASTUS) // not really used but has to be set anyway :(
-                    .faces()
-                    .detectWithUrl()
-                    .withUrl(input.getUrl())
-                    .withReturnFaceAttributes(attributes)
-                    .withReturnFaceLandmarks(false)
-                    .withReturnFaceId(false)
-                    .executeAsync()
-                    .subscribe(detectedFaces -> {
-                        try {
-                            Output[] out = detectedFaces
-                                    .stream()
-                                    .map(face -> new Output(input.getChat(), face.faceAttributes().age(), face.faceAttributes().emotion()))
-                                    .toArray(Output[]::new);
-                            emitter.complete(out);
-                        } catch (Exception e) {
-                            emitter.fail(e);
-                        }
-                    }, throwable -> {
-                        emitter.fail(throwable);
-                    });
+
+    @Funq
+    public Uni<Output[]> function(Input input, @Context CloudEvent cloudEvent) {
+        return Uni.createFrom().emitter(emitter -> {
+            getData(input, cloudEvent, emitter, 100);
         });
 
+    }
+
+    private void getData(Input input, CloudEvent cloudEvent, UniEmitter<? super Output[]> emitter, int retries) {
+        if (retries <= 0) {
+            emitter.fail(new RuntimeException("Too many fails to call face api."));
+        }
+        FaceAPI faceAPI = FaceAPIManager.authenticate("https://boson.cognitiveservices.azure.com/face/v1.0", API_KEY);
+        faceAPI
+                .withAzureRegion(AzureRegions.EASTUS) // not really used but has to be set anyway :(
+                .faces()
+                .detectWithUrl()
+                .withUrl(input.getUrl())
+                .withReturnFaceAttributes(attributes)
+                .withReturnFaceLandmarks(false)
+                .withReturnFaceId(false)
+                .executeAsync()
+                .subscribe(detectedFaces -> {
+                    try {
+                        Output[] out = detectedFaces
+                                .stream()
+                                .map(face -> new Output(input.getChat(), face.faceAttributes().age(), face.faceAttributes().emotion()))
+                                .toArray(Output[]::new);
+                        emitter.complete(out);
+                    } catch (Exception e) {
+                        emitter.fail(e);
+                    }
+                }, throwable -> {
+                    if (throwable instanceof UnknownHostException || throwable instanceof ConnectException) {
+                        vertx.setTimer(100, dummy -> getData(input, cloudEvent, emitter, retries-1));
+                    } else {
+                        emitter.fail(throwable);
+                    }
+                });
     }
 }
